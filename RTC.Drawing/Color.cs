a7b9efaafdfba88549decc9be796dd7351872a8a
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-04-22 10:50:51 +0200

    Add color class + tests

---
+++ RTC.Drawing/Color.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace RTC.Drawing
+{
+  public class Color
+  {
+    private const double Epsilon = 0.00001;
+
+    private readonly double[] _color = { 0.0, 0.0, 0.0 };
+
+    public Color(double r, double g, double b)
+    {
+      _color[0] = r;
+      _color[1] = g;
+      _color[2] = b;
+    }
+
+    public double Red
+    {
+      get => _color[0];
+      set => _color[0] = value;
+    }
+
+    public double Green
+    {
+      get => _color[1];
+      set => _color[1] = value;
+    }
+
+    public double Blue
+    {
+      get => _color[2];
+      set => _color[2] = value;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (obj is Color)
+        return this == (Color)obj;
+
+      return false;
+    }
+
+    public override int GetHashCode()
+    {
+      return base.GetHashCode();
+    }
+
+    public static bool operator ==(Color c1, Color c2)
+    {
+      return Math.Abs(c1._color[0] - c2._color[0]) < Epsilon
+        && Math.Abs(c1._color[1] - c2._color[1]) < Epsilon
+        && Math.Abs(c1._color[2] - c2._color[2]) < Epsilon;
+    }
+
+    public static bool operator !=(Color c1, Color c2) => !(c1 == c2);
+
+    public static Color operator +(Color c1, Color c2)
+    {
+      return new Color(
+        c1._color[0] + c2._color[0],
+        c1._color[1] + c2._color[1],
+        c1._color[2] + c2._color[2]);
+    }
+
+    public static Color operator -(Color c1, Color c2)
+    {
+      return new Color(
+        c1._color[0] - c2._color[0],
+        c1._color[1] - c2._color[1],
+        c1._color[2] - c2._color[2]);
+    }
+
+    public static Color operator *(double factor, Color c)
+    {
+      return new Color(
+        factor * c._color[0],
+        factor * c._color[1],
+        factor * c._color[2]);
+    }
+
+    public static Color operator *(Color c1, Color c2)
+    {
+      // Hadamard product (or Schur product)
+      return new Color(
+        c1._color[0] * c2._color[0],
+        c1._color[1] * c2._color[1],
+        c1._color[2] * c2._color[2]);
+    }
+  }
+}
---
+++ RTC.Tests/TestColor.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using RTC.Drawing;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing
+{
+  [TestFixture]
+  public class TestColor
+  {
+    private const double Epsilon = 0.00001;
+
+    [Test]
+    public void TestNewColor()
+    {
+      var c = new Color(-0.5, 0.4, 1.7);
+
+      Assert.AreEqual(-0.5, c.Red, Epsilon);
+      Assert.AreEqual(0.4, c.Green, Epsilon);
+      Assert.AreEqual(1.7, c.Blue, Epsilon);
+    }
+
+    [Test]
+    public void TestAddColor()
+    {
+      var c1 = new Color(0.9, 0.6, 0.75);
+      var c2 = new Color(0.7, 0.1, 0.25);
+
+      Assert.AreEqual(c1 + c2, new Color(1.6, 0.7, 1.0));
+    }
+
+    [Test]
+    public void TestSubtractColor()
+    {
+      var c1 = new Color(0.9, 0.6, 0.75);
+      var c2 = new Color(0.7, 0.1, 0.25);
+
+      Assert.AreEqual(c1 - c2, new Color(0.2, 0.5, 0.5));
+    }
+
+    [Test]
+    public void TestMultiplyColor()
+    {
+      var c = new Color(0.2, 0.3, 0.4);
+
+      Assert.AreEqual(2 * c, new Color(0.4, 0.6, 0.8));
+    }
+
+    [Test]
+    public void TestMultiplyColors()
+    {
+      var c1 = new Color(1, 0.2, 0.4);
+      var c2 = new Color(0.9, 1, 0.1);
+
+      Assert.AreEqual(c1 * c2, new Color(0.9, 0.2, 0.04));
+    }
+  }
+}
