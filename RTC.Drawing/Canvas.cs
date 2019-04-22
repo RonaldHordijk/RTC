231ac5317c47042e1d224e0e087d5b60141ae8e3
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-04-22 11:23:11 +0200

    Add canvas

---
+++ RTC.Drawing/Canvas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTC.Drawing
+{
+    public class Canvas
+    {
+        private readonly Color[,] _pixels;
+        public int Width { get; }
+        public int Height { get;}
+
+        public Canvas(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            _pixels = new Color[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    _pixels[x, y] = new Color(0, 0, 0);
+                }
+            }
+        }
+
+        public Color PixelAt(int x, int y) => _pixels[x, y];
+        public void SetPixel(int x, int y, Color c) => _pixels[x, y] = c;
+    }
+}
---
+++ RTC.Tests/CanvasTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using RTC.Drawing;
+
+namespace Drawing
+{
+    [TestFixture]
+    public class CanvasTests
+    {
+        [Test]
+        public void TestNewCanvas()
+        {
+            var canvas = new Canvas(10, 20);
+
+            Assert.AreEqual(10, canvas.Width);
+            Assert.AreEqual(20, canvas.Height);
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    Assert.AreEqual(canvas.PixelAt(x, y), new Color(0, 0, 0));
+                }
+            }
+        }
+
+        [Test]
+        public void TestSetPixel()
+        {
+            var canvas = new Canvas(10, 20);
+            var red = new Color(1, 0, 0);
+
+            canvas.SetPixel(2, 3, red);
+
+            Assert.AreEqual(canvas.PixelAt(2, 3), new Color(1, 0, 0));
+        }
+    }
+}
