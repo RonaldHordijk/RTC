de630ac1e18b60f1b1b573cfff7aeb594cde1ed0
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-09-15 19:17:55 +0200

    Initial version of cubes

---
+++ RTC.Geometry.Objects/Shapes/Cube.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RTC.Geometry.Objects.Shapes
+{
+    public class Cube : Shape
+    {
+        private const double Epsilon = 0.00001;
+
+        private (double, double) CheckAxis(double origin, double dir)
+        {
+            var tmin = -1 - origin;
+            var tmax = 1 - origin;
+
+            if (Math.Abs(dir) < Epsilon)
+            {
+                tmin *= 1E9;
+                tmax *= 1E9;
+            }
+
+            tmin /= dir;
+            tmax /= dir;
+
+            if (tmin < tmax)
+                return (tmin, tmax);
+
+            return (tmax, tmin);
+        }
+
+
+        protected override Intersections LocalIntersection(Ray rayTransformed)
+        {
+            (double xtmin, double xtmax) = CheckAxis(rayTransformed.Origin.X, rayTransformed.Direction.X);
+            (double ytmin, double ytmax) = CheckAxis(rayTransformed.Origin.Y, rayTransformed.Direction.Y);
+            (double ztmin, double ztmax) = CheckAxis(rayTransformed.Origin.Z, rayTransformed.Direction.Z);
+
+            double tmin = Math.Max(xtmin, Math.Max(ytmin, ztmin));
+            double tmax = Math.Min(xtmax, Math.Min(ytmax, ztmax));
+
+            if (tmin > tmax)
+                return new Intersections();
+
+            return new Intersections
+            {
+                new Intersection(tmin, this),
+                new Intersection(tmax, this)
+            };
+        }
+
+        protected override Tuple LocalNormal(Tuple objectPoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RTC.Tests/TestCube.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using RTC.Geometry;
+using RTC.Geometry.Objects;
+using RTC.Geometry.Objects.Shapes;
+
+namespace RTC.Tests
+{
+    [TestFixture]
+    public class TestCube
+    {
+        [Test]
+        [TestCase(5, 0.5, 0, -1, 0, 0, 4, 6)]
+        [TestCase(-5, 0.5, 0, 1, 0, 0, 4, 6)]
+        [TestCase(0.5, 5, 0, 0, -1, 0, 4, 6)]
+        [TestCase(0.5, -5, 0, 0, 1, 0, 4, 6)]
+        [TestCase(0.5, 0, 5, 0, 0, -1, 4, 6)]
+        [TestCase(0.5, 0, -5, 0, 0, 1, 4, 6)]
+        [TestCase(0, 0.5, 0, 0, 0, 1, -1, 1)]
+
+        public void TestIntersectCube(double o1, double o2, double o3, double d1, double d2, double d3, double r1, double r2)
+        {
+            var cube = new Cube();
+            var ray = new Ray(Tuple.Point(o1, o2, o3), Tuple.Vector(d1, d2, d3));
+            var xs = cube.Intersect(ray);
+
+            Assert.AreEqual(2, xs.Count);
+            Assert.AreEqual(r1, xs[0].Distance);
+            Assert.AreEqual(r2, xs[1].Distance);
+        }
+
+        [Test]
+        [TestCase(-2, 0, 0 , 0.2673, 0.5345, 0.8018)]
+        [TestCase(0, -2, 0, 0.8018, 0.2673, 0.5345)]
+        [TestCase(0, 0, -2, 0.5345, 0.8018, 0.2673)]
+        [TestCase(2, 0, 2, 0, 0, -1)]
+        [TestCase(0, 2, 2, 0, -1, 0)]
+        [TestCase(2, 2, 0, -1, 0, 0)]
+        public void TestIntersectionmissesCube(double o1, double o2, double o3, double d1, double d2, double d3)
+        {
+            var cube = new Cube();
+            var ray = new Ray(Tuple.Point(o1, o2, o3), Tuple.Vector(d1, d2, d3));
+            var xs = cube.Intersect(ray);
+
+            Assert.AreEqual(0, xs.Count);
+        }
+    }
+}
